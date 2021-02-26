d12f13d23a7883532925e284ee2f96b823568f7d
Agent-Hellboy <princekrroshan01@gmail.com>
2021-02-27 01:04:13 +0530

    Added docs and CI

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter
+using FileType
+
+makedocs(
+    sitename = "FileType",
+    format = Documenter.HTML(),
+    modules = [FileType]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "https://github.com/Agent-Hellboy/FileType.jl.git",devbranch = "main
+)
---
+++ docs/src/index.md
@@ -0,0 +1,18 @@
+# FileType.jl
+
+Documentation for FileType.jl
+
+
+# Documentation
+
+```@meta
+CurrentModule = FileType
+DocTestSetup = quote
+    using FileType
+end
+```
+
+```@autodocs
+Modules = [FileType]
+```
+
